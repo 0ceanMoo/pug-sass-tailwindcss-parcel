5aefe2ecd3ae18f54842702fa01d6776ffd3b568
Wataru <wakutsu@d-r.co.jp>
2021-09-08 22:37:20 +0900

    設定ファイルなどを追加

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './src/**/*.pug',
+    './src/**/*.sass',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
